27d2fe86b8bf61b1055aa8f79aa3984efd617fe1
Norrius <norrius@gmail.com>
2014-11-24 05:45:53 +0400

    Some suffix tree implementation.

--- SuffixTree/main.cpp
+++ SuffixTree/main.cpp
@@ -1,10 +1,27 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include "suffixtree.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int k;
+    cin >> k;
+    while (k--) {
+        string s;
+        cin >> s;
+        SuffixTree tree(s);
+        cerr << "!";
+
+        int q;
+        cin >> q;
+        while (q--) {
+            string t;
+            cin >> t;
+            cout << (tree.contains(t) ? "y" : "n") << '\n';
+        }
+    }
+
     return 0;
 }
 
--- SuffixTree/suffixtree.h
+++ SuffixTree/suffixtree.h
@@ -1,11 +1,60 @@
 #ifndef SUFFIXTREE_H
 #define SUFFIXTREE_H
+#include <vector>
 #include <string>
+#include <limits>
+#include <unordered_map>
+#include <map>
+
+const size_t infty = std::numeric_limits<size_t>::max();
 
 class SuffixTree
 {
+    struct NodeLink {
+        size_t index;
+        size_t left;
+        size_t right;
+        NodeLink() {}
+        NodeLink(size_t index, size_t left, size_t right) :
+            index(index), left(left), right(right) {}
+    };
+
+    struct ReferencePair {
+        size_t closest;
+        size_t left;
+        size_t right;
+        ReferencePair() {}
+        ReferencePair(size_t closest, size_t left, size_t right) :
+            closest(closest), left(left), right(right) {}
+    };
+
+    struct Node {
+//        size_t left;
+//        size_t right;
+//        size_t parent;
+        size_t sufflink;
+        std::/*unordered_*/map<char, NodeLink> links;
+        Node() {}
+//        Node(size_t left, size_t right, size_t sufflink) :
+//            left(left), right(right), sufflink(sufflink) {}
+        bool hasLink(char c);
+    };
+
+    const std::string string;
+    size_t root;
+    size_t dummy;
+    std::vector<Node> nodes;
+    ReferencePair activePoint;
+
+    void addNextSymbol();
+    void canonicalizeReference(ReferencePair *node);
+    size_t testAndSplit();
+
+    void buildTree();
+
 public:
-    SuffixTree(std::string string);
+    SuffixTree(const std::string &string);
+    bool contains(const std::string &substring);
 };
 
 #endif // SUFFIXTREE_H
